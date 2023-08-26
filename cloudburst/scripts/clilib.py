dddd9989642d3a1badd6f2e925dd7fb8aeaff223
Albert Kottke <albert.kottke@gmail.com>
2023-08-26 10:25:19 -0700

    Ran black on code.

--- cloudburst/scripts/clilib.py
+++ cloudburst/scripts/clilib.py
@@ -1,10 +1,20 @@
 ﻿# cloudburst framework - Bruce Hearn 2021 bruce.hearn@gmail.com
+
 import os
+
 from pathlib import Path
-import s3lib, fwlib, ziplib
+
+import s3lib
+import ziplib
+
 
 # this will compress folders in the input folder into zip files
-def compress_inputs(source_folder:Path = 'source.tmp/', zip_folder : Path = 'zipped.tmp/', level:int = 1, filter:str = None):
+def compress_inputs(
+    source_folder: Path = "source.tmp/",
+    zip_folder: Path = "zipped.tmp/",
+    level: int = 1,
+    filter: str = None,
+):
     if not (os.path.exists(source_folder)):
         print("Cannot find inputFolder {0}, exiting".format(source_folder))
         exit(-1)
@@ -14,49 +24,62 @@ def compress_inputs(source_folder:Path = 'source.tmp/', zip_folder : Path = 'zip
 
     for dir in source_folder.rglob(filter):
         if dir.is_dir():
-            out_file = f'{str(dir).replace(str(source_folder), str(zip_folder))}.7z'
+            out_file = f"{str(dir).replace(str(source_folder), str(zip_folder))}.7z"
             print(out_file)
-            
+
             if os.path.exists(out_file):
                 os.remove(out_file)
-            
+
             ziplib.compress(source_path=dir, zip_path=out_file)
-            
-# load the data into S3 
+
+
+# load the data into S3
 def upload(
     bucket_name: str,
-    local_folder: Path = Path('./input.tmp/'),
-    prefix:str = 'input/',
-    filter:str = '*',
-    thread_count : int = 10):
+    local_folder: Path = Path("./input.tmp/"),
+    prefix: str = "input/",
+    filter: str = "*",
+    thread_count: int = 10,
+):
+    # load the bucket with files from a local directory. Exclude directory names!
+    print(
+        f"loading input data to bucket: s3://{bucket_name}/{prefix} from {local_folder}"
+    )
+    s3lib.put_files(
+        bucket_name=bucket_name,
+        local_folder=local_folder,
+        prefix=prefix,
+        filter=filter,
+        threads=thread_count,
+    )
 
-    #load the bucket with files from a local directory. Exclude directory names!
-    print(f'loading input data to bucket: s3://{bucket_name}/{prefix} from {local_folder}')
-    s3lib.put_files(bucket_name=bucket_name, local_folder=local_folder, prefix=prefix, filter=filter, threads=thread_count)
 
-def expand_outputs(local_path ='./output.tmp/', output_dir ='', test:bool=False, remove_zip:bool=False):
-	# unzip all the zips in the download folder
+def expand_outputs(
+    local_path="./output.tmp/",
+    output_dir="",
+    test: bool = False,
+    remove_zip: bool = False,
+):
+    # unzip all the zips in the download folder
     items = list(Path(local_path).rglob("*.7[zZ]"))
     n = 0
     for item in items:
         n += 1
         s_item = str(item)
 
-        if output_dir == '':
+        if output_dir == "":
             my_out_dir = str(item.parent)
         else:
             my_out_dir = output_dir
 
-        if item.name.startswith('Logs'): # handle log directories a little differently
-            destination_path = os.path.join(my_out_dir, item.stem)
-        else:
-            destination_path = my_out_dir
-        
+        if item.name.startswith("Logs"):  # handle log directories a little differently
+            my_out_dir = os.path.join(my_out_dir, item.stem)
+
         if test:
             print("test - unzip {0} to {1}".format(s_item, my_out_dir))
             ret = 1
         else:
-            ret = ziplib.expand(source_path= s_item, destination_path= my_out_dir)
+            ret = ziplib.expand(source_path=s_item, destination_path=my_out_dir)
 
         if ret == 0:
             if remove_zip:
@@ -67,21 +90,35 @@ def expand_outputs(local_path ='./output.tmp/', output_dir ='', test:bool=False,
     if n == 0:
         print("no 7z files found under: " + local_path)
 
-def get(bucket:str, prefix:str, filter:str, local_folder='output.tmp', thread_count:int=10):
+
+def get(
+    bucket: str,
+    prefix: str,
+    filter: str,
+    local_folder="output.tmp",
+    thread_count: int = 10,
+):
     os.makedirs(local_folder, exist_ok=True)
 
-    print(f'getting data from bucket: s3://{bucket}/{prefix} from {local_folder}')
-    s3lib.get_files(bucket_name=bucket, local_path=local_folder, prefix=prefix, filter=filter, threads=thread_count)
+    print(f"getting data from bucket: s3://{bucket}/{prefix} from {local_folder}")
+    s3lib.get_files(
+        bucket_name=bucket,
+        local_path=local_folder,
+        prefix=prefix,
+        filter=filter,
+        threads=thread_count,
+    )
+
 
 # convert an input string e.g. comma separated list or a range to a padded list
-def convert_input_to_list(input_string:str, padding:int):
+def convert_input_to_list(input_string: str, padding: int):
     items = []
-    b_is_range = '-' in input_string
+    b_is_range = "-" in input_string
     if b_is_range:
-        items = input_string.split('-')
+        items = input_string.split("-")
     else:
-        items = input_string.split(',')
-    
+        items = input_string.split(",")
+
     output_items = []
 
     # convert the range to a list, assumes that the numbers are continuous
@@ -91,7 +128,7 @@ def convert_input_to_list(input_string:str, padding:int):
         i_last = int(items[1])
 
         # build a list of sites
-        for i in range(i_first, i_last+1):
+        for i in range(i_first, i_last + 1):
             item = str(i).zfill(padding)
             output_items.append(item)
     else:
--- cloudburst/scripts/s3lib.py
+++ cloudburst/scripts/s3lib.py
@@ -12,7 +12,7 @@ def copy_s3_object(bucket_name, key, local_file):
     print(f"fetching file: s3://{bucket_name}/{key} to {local_file}")
     os.makedirs(os.path.dirname(local_file), exist_ok=True)
 
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     try:
         s3.download_file(bucket_name, key, local_file)
         return 0
@@ -20,39 +20,46 @@ def copy_s3_object(bucket_name, key, local_file):
         print(f"Error fetching s3://{bucket_name}/{key} to {local_file}: {str(err)}")
         return 213
 
+
 def list_objects(bucket_name, client, prefix):
     ar = []
     files = client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
-    if 'Contents' in files:
-        for obj in files['Contents']:
-            file = obj['Key']
+    if "Contents" in files:
+        for obj in files["Contents"]:
+            file = obj["Key"]
             ar.append(file)
     return ar
+
 
 def copy_s3_objects(bucket_name, prefix, local_folder):
     print(f"fetching files: s3://{bucket_name}/{prefix} to {local_folder}")
     os.makedirs(local_folder, exist_ok=True)
 
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     try:
         files = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
-        for obj in files['Contents']:
-            file = obj['Key']
+        for obj in files["Contents"]:
+            file = obj["Key"]
             local_file = file.replace(prefix, local_folder)
             s3.download_file(bucket_name, file, local_file)
         return 0
     except Exception as err:
-        print("error copying s3://{0}/{1} to {2}: {3}".format(bucket_name, prefix, local_folder, str(err)))
+        print(
+            "error copying s3://{0}/{1} to {2}: {3}".format(
+                bucket_name, prefix, local_folder, str(err)
+            )
+        )
         return 214
 
-# write an object to S3, with error handling
-def write_s3_object(bucket_name:str, key:str, file:Path):
 
-    print (f"storing {file} to s3://{bucket_name}/{key}")
+# write an object to S3, with error handling
+def write_s3_object(bucket_name: str, key: str, file: Path):
+    print(f"storing {file} to s3://{bucket_name}/{key}")
     session = boto3.Session()
-    client = session.client('s3')
+    client = session.client("s3")
 
     return put_file(bucket_name, client=client, local_file=Path(file), key=key)
+
 
 # delegate to download a single file, used by multi-threader in getFiles()
 def put_file(bucket: str, client: boto3.client, local_file: Path, key: str):
@@ -70,49 +77,60 @@ def put_file(bucket: str, client: boto3.client, local_file: Path, key: str):
         client.upload_file(Bucket=bucket, Key=key, Filename=str(local_file))
     except BaseException as ex:
         return_code = 215
-        print(f'failed to upload {local_file}: {str(ex)}')
+        print(f"failed to upload {local_file}: {str(ex)}")
 
     return return_code
 
+
 # multi-threaded impl: files in folder to s3 bucket/prefix/
-def put_files(bucket_name:str, local_folder:Path, prefix:str, filter:str = '*', threads:int = 10):
+def put_files(
+    bucket_name: str,
+    local_folder: Path,
+    prefix: str,
+    filter: str = "*",
+    threads: int = 10,
+):
     return_code = 0
 
-    print (f'storing files: {local_folder} to s3://{bucket_name}/{prefix}')
+    print(f"storing files: {local_folder} to s3://{bucket_name}/{prefix}")
     session = boto3.Session()
-    client = session.client('s3')
+    client = session.client("s3")
 
     # Path ignores preceding './'. Remove it so we can substitute accurately
     local_folder = str(local_folder)
-    if len(local_folder) > 2 and str(local_folder).startswith('./'):
+    if len(local_folder) > 2 and str(local_folder).startswith("./"):
         local_folder = str(local_folder)[2:]
 
     futures = []
     try:
         with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
             for file in Path(local_folder).rglob(filter):
-                if file.is_file() and not 'DS_Store' in file.name:
+                if file.is_file() and not "DS_Store" in file.name:
                     if str(file).startswith(str(local_folder)):
-                        tmp = str(file)[len(str(local_folder)):]
-                        if len(str(prefix)) > 0 and prefix[len(str(prefix))-1] != '/':
-                            prefix = prefix + '/'
+                        tmp = str(file)[len(str(local_folder)) :]
+                        if len(str(prefix)) > 0 and prefix[len(str(prefix)) - 1] != "/":
+                            prefix = prefix + "/"
                         key = prefix + tmp
-                    key = key.replace('//', '/')
-                    futures.append(executor.submit(put_file, bucket_name, client, file, key))
+                    key = key.replace("//", "/")
+                    futures.append(
+                        executor.submit(put_file, bucket_name, client, file, key)
+                    )
 
             for future in concurrent.futures.as_completed(futures):
                 process_result = future.result()
                 if process_result != 0:
                     return_code = process_result
     except Exception as err:
-        print(f'error copying {local_folder} to s3://{bucket_name}/{prefix}: {str(err)}')
+        print(
+            f"error copying {local_folder} to s3://{bucket_name}/{prefix}: {str(err)}"
+        )
         return_code = 216
 
     return return_code
 
 
 # delegate to download a single file, used by multi-threader in getFiles()
-def get_file(bucket:str, local_file:str, client: boto3.client, key:str):
+def get_file(bucket: str, local_file: str, client: boto3.client, key: str):
     return_code = 0
     """
     Download a single file from S3
@@ -121,7 +139,7 @@ def get_file(bucket:str, local_file:str, client: boto3.client, key:str):
         output (str): Dir to store the images
         client (boto3.client): S3 client
         s3_file (str): S3 object name
-    """ 
+    """
     try:
         # print(f'downloading: {local_file} from s3://{bucket}/{key}')
         os.makedirs(os.path.dirname(local_file), exist_ok=True)
@@ -130,12 +148,12 @@ def get_file(bucket:str, local_file:str, client: boto3.client, key:str):
     except BaseException as ex:
         print(f"failed to download {local_file} from {key}: {str(ex)}")
         return_code = 217
-    
+
     return return_code
 
 
 # multithreaded implementation
-def get_files_list(bucket_name : str, keys : list, local_folder: str, threads : int = 10):
+def get_files_list(bucket_name: str, keys: list, local_folder: str, threads: int = 10):
     return_code = 0
     # Creating only one session and one client
     session = boto3.Session()
@@ -150,24 +168,31 @@ def get_files_list(bucket_name : str, keys : list, local_folder: str, threads : 
             futures.append(future)
 
         for future in concurrent.futures.as_completed(futures):
-                process_result = future.result()
-                if process_result != 0:
-                    return_code = process_result
+            process_result = future.result()
+            if process_result != 0:
+                return_code = process_result
 
     return return_code
 
-def get_files(bucket_name:str, prefix:str, local_path:str, filter:str = None, threads:int = 10):
+
+def get_files(
+    bucket_name: str,
+    prefix: str,
+    local_path: str,
+    filter: str = None,
+    threads: int = 10,
+):
     return_code = 0
     # create only one session and one client
     session = boto3.Session()
     client = session.client("s3")
-    print(f'fetching: {local_path} from s3://{bucket_name}/{prefix}')
+    print(f"fetching: {local_path} from s3://{bucket_name}/{prefix}")
 
     the_local_path = Path(local_path)
 
     keys = []
     for key in list_objects(bucket_name=bucket_name, client=client, prefix=prefix):
-        if filter is None or filter == '' or re.search(filter, key):
+        if filter is None or filter == "" or re.search(filter, key):
             keys.append(key)
 
     # print (f"len of keys: {len(keys)}")
@@ -193,28 +218,30 @@ def get_files(bucket_name:str, prefix:str, local_path:str, filter:str = None, th
         futures = []
         with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
             for key in keys:
-                if filter is None or filter == '' or re.search(filter, key):
+                if filter is None or filter == "" or re.search(filter, key):
                     # get last index of '/'
                     if len(prefix) == 0:
                         local_file = the_local_path.joinpath(key)
                     else:
                         tmp_prefix = prefix
-                        if prefix[-1] != '/':
+                        if prefix[-1] != "/":
                             tmp_prefix = os.path.dirname(prefix)
-                        rel_key = key.replace(tmp_prefix,'')
-                        if rel_key[0] == '/':
+                        rel_key = key.replace(tmp_prefix, "")
+                        if rel_key[0] == "/":
                             rel_key = rel_key[1:]
                         local_file = the_local_path.joinpath(rel_key)
 
                     # print(f'prefix: {prefix}  tmp_prefix: {tmp_prefix} rel_key: {rel_key} local_file {local_file}')
-                    futures.append(executor.submit(get_file, bucket_name, local_file, client, key))
+                    futures.append(
+                        executor.submit(get_file, bucket_name, local_file, client, key)
+                    )
 
             for future in concurrent.futures.as_completed(futures):
-                    process_result = future.result()
-                    if process_result != 0:
-                        return_code = process_result
+                process_result = future.result()
+                if process_result != 0:
+                    return_code = process_result
     else:
-        print(f"error: no object found at s3://{bucket}/{key}")
+        print(f"error: no object found at s3://{bucket_name}/{prefix}")
         return_code = 218
 
     return return_code
